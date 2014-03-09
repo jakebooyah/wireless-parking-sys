a74ba870253046799ba6b0f3772b1f2beda909b7
modomodo <mugz.mike@gmail.com>
2014-03-10 00:52:45 +0800

    Added search action

--- Codes/UI/src/com/example/userinterface/MainActivity.java
+++ Codes/UI/src/com/example/userinterface/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.userinterface;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -15,8 +16,23 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.main_actionbar, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		//Handle presses on the action bar items
+		switch(item.getItemId()){
+		case R.id.action_search:
+			//openSearch();
+			return true;
+		case R.id.action_settings:
+			//openSettings();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 }
