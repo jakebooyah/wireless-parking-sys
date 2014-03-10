e3f8e5ffe50a4a2c9dad21994bd27a56d7875d64
modomodo <mugz.mike@gmail.com>
2014-03-10 19:20:52 +0800

    Update

--- Codes/UI/src/com/example/userinterface/MainActivity.java
+++ Codes/UI/src/com/example/userinterface/MainActivity.java
@@ -10,7 +10,8 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.main_grid);
+		getActionBar().setHomeButtonEnabled(true);
 	}
 
 	@Override
