bab191f9298a39e7fb32c57469f75de37288420b
modomodo <mugz.mike@gmail.com>
2014-03-13 09:35:33 +0800

    Fixed Tabs No Change + Delete Unnecessary layout

--- Codes/UI/src/com/example/userinterface/MapActivity.java
+++ Codes/UI/src/com/example/userinterface/MapActivity.java
@@ -1,17 +1,18 @@
 package com.example.userinterface;
 
 import android.app.ActionBar;
-import android.app.ActionBar.Tab;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
-public class MapActivity extends FragmentActivity implements ActionBar.TabListener {
-
-	private ViewPager viewPager;
-    private TabPagerAdapter mAdapter;
-    private ActionBar actionBar;
+public class MapActivity extends FragmentActivity {
+	
+	//Declaration of resources to be used
+	ViewPager Tab;
+    TabPagerAdapter TabAdapter;
+    ActionBar actionBar;
+    
     // Tab titles
     private String[] tabs = { "All", "Zone A", "Zone B" };
  
@@ -21,37 +22,50 @@ public class MapActivity extends FragmentActivity implements ActionBar.TabListen
         setContentView(R.layout.map_section);
     
         // Initilization
-        viewPager = (ViewPager) findViewById(R.id.pager);
+        Tab = (ViewPager) findViewById(R.id.pager);
+        TabAdapter = new TabPagerAdapter(getSupportFragmentManager());
+ 
+        Tab.setOnPageChangeListener(
+        		new ViewPager.SimpleOnPageChangeListener(){
+        			@Override
+        			public void onPageSelected(int position) {
+        				actionBar = getActionBar();
+        				actionBar.setSelectedNavigationItem(position);                    }
+        		}
+        );
+     
+        Tab.setAdapter(TabAdapter);
+        
+        //Set the action bar
         actionBar = getActionBar();
-        mAdapter = new TabPagerAdapter(getSupportFragmentManager());
- 
-        viewPager.setAdapter(mAdapter);
-        actionBar.setHomeButtonEnabled(false);
+        
+        //Enable tabs on action Bar and add a listener
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);        
- 
+        
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {	
+			@Override
+			public void onTabUnselected(android.app.ActionBar.Tab tab, FragmentTransaction ft) {
+				// TODO Auto-generated method stub
+			}
+			
+			@Override
+			public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+				// TODO Auto-generated method stub
+				//Return the current position of the tab in the action bar
+				Tab.setCurrentItem(tab.getPosition());
+			}
+			
+			@Override
+			public void onTabReselected(android.app.ActionBar.Tab tab, FragmentTransaction ft) {
+				// TODO Auto-generated method stub		
+			}
+		};
         // Adding Tabs
-        for (String tab_name : tabs) {
-            actionBar.addTab(actionBar.newTab().setText(tab_name)
-                    .setTabListener(this));
+         actionBar.addTab(actionBar.newTab().setText(tabs[0]).setTabListener(tabListener)) ;
+         actionBar.addTab(actionBar.newTab().setText(tabs[1]).setTabListener(tabListener)) ;    
+         actionBar.addTab(actionBar.newTab().setText(tabs[2]).setTabListener(tabListener)) ;
+    }
 }
-}
 
 
-	@Override
-	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onTabSelected(Tab arg0, FragmentTransaction arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-   }
+	
