dee763a948a5ac9c2a3a87995ee5f02bff86c213
zhien <zelim92@gmail.com>
2014-04-05 18:11:59 +0800

    ui-find

--- Codes/UI/src/com/example/userinterface/MapFragment.java
+++ Codes/UI/src/com/example/userinterface/MapFragment.java
@@ -3,8 +3,12 @@ package com.example.userinterface;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MapFragment extends Fragment {
 	
@@ -12,8 +16,30 @@ public class MapFragment extends Fragment {
             Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        
+        WebView myWebView = (WebView) rootView.findViewById(R.id.webview);
+        String url = "http://ec2-54-254-255-187.ap-southeast-1.compute.amazonaws.com/grp";
+
+        WebSettings webSettings = myWebView.getSettings();
+        myWebView.setInitialScale(1);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setJavaScriptEnabled(true);
+
+        myWebView.setWebViewClient(new Callback());  //HERE IS THE MAIN CHANGE
+        myWebView.loadUrl(url);
          
         return rootView;
+    }
+	
+	private class Callback extends WebViewClient{  //HERE IS THE MAIN CHANGE. 
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return (false);
+        }
+
     }
 }
 
